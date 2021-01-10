61e3653224e76a78f74865ddb0405b69f2f6bfc7
Artem <artemp122848@gmail.com>
2021-01-10 21:31:22 +0400

    hello world

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*int a= 1;
+	int b=2;
+	int c=a+b;
+	System.out.println("a > b= "+ (a>b));
+        System.out.println("a < b= "+ (a<b));*/
+        String Hello = "Hello";
+        String world = "world";
+        System.out.println(Hello+" "+world+"!");
+
+
+    }
+}
